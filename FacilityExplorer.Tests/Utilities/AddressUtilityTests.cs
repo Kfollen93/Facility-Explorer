9f5d97276f681387633ea0d75a5357ba16b166bb
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2023-12-13 16:39:39 -0800

    Add address validation tests

--- FacilityExplorer.Tests/Utilities/AddressUtilityTests.cs
+++ FacilityExplorer.Tests/Utilities/AddressUtilityTests.cs
@@ -6,6 +6,35 @@ namespace FacilityExplorer.Tests.Utilities
     public class AddressUtilityTests
     {
         [Fact]
+        public void IsAddressFormatValid_ValidAddress_ReturnsTrue()
+        {
+            // Arrange
+            string validAddress = "123 Main St, Cityville, CA 12345";
+
+            // Act
+            bool result = AddressUtility.IsAddressFormatValid(validAddress);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("InvalidAddress")]
+        [InlineData("Street, City")]
+        [InlineData("Street, City, State")]
+        [InlineData("Street, City, State, 12345")]
+        [InlineData("Street, City, State 123456")]
+        public void IsAddressFormatValid_InvalidAddresses_ReturnsFalse(string invalidAddress)
+        {
+            // Act
+            bool result = AddressUtility.IsAddressFormatValid(invalidAddress);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
         public void CreateAddress_ValidInput_CreatesCorrectAddress()
         {
             // Arrange
