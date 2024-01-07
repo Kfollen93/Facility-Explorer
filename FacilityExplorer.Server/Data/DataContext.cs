8048d69ab45a6d309edb249093dc4d004be26ae4
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2024-01-06 16:16:20 -0800

    Update to context to inherit from Identity

--- FacilityExplorer.Server/Data/DataContext.cs
+++ FacilityExplorer.Server/Data/DataContext.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using FacilityExplorer.Server.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace FacilityExplorer.Server.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        { 
-        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Facility> Facilities => Set<Facility>();
 
