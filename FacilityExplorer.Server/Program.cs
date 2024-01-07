6e6e559b9d75c8a917d31fb99948e537c7b26c42
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2024-01-06 16:17:56 -0800

    Update with identity roles

--- FacilityExplorer.Server/Program.cs
+++ FacilityExplorer.Server/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.EntityFrameworkCore;
 using FacilityExplorer.Server.Data;
 using FacilityExplorer.Server.Repositories.FacilityRepository;
 using FacilityExplorer.Server.Middlewares;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +16,23 @@ builder.Services.AddLogging();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<DataContext>();
 
 builder.Services.AddCors(options =>
 {
@@ -37,29 +54,68 @@ app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    Console.WriteLine("WE IN DEV");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-
+app.MapIdentityApi<IdentityUser>();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.UseMiddleware<GlobalExceptionHandling>();
-
 app.MapControllers();
-
 app.MapFallbackToFile("/index.html");
 
 // Seed Database
 using (var serviceScope = app.Services.CreateScope())
 {
-    var context = serviceScope.ServiceProvider.GetRequiredService<DataContext>();
-    DbSeeder.ClearData(context);
-    DbSeeder.SeedData(context);
+    await PopulateDummyData(serviceScope);
+    await CreateRoles(serviceScope);
+    await CreateAdminAccount(serviceScope);
 }
+
+
 
 app.Run();
 
+static async Task CreateAdminAccount(IServiceScope serviceScope)
+{
+    var configuration = serviceScope.ServiceProvider.GetRequiredService<IConfiguration>();
+    var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    var adminCredentials = configuration.GetSection("AdminCredentials");
+    string? email = adminCredentials["Email"];
+    string password = adminCredentials["Password"]!;
+
+    if (await userManager.FindByEmailAsync(email) == null)
+    {
+        var user = new IdentityUser()
+        {
+            UserName = email,
+            Email = email,
+        };
+
+        await userManager.CreateAsync(user, password);
+        await userManager.AddToRoleAsync(user, "Admin");
+    }
+}
+
+static async Task CreateRoles(IServiceScope serviceScope)
+{
+    var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var roles = new[] { "Admin", "User" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+}
+
+static async Task PopulateDummyData(IServiceScope serviceScope)
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<DataContext>();
+    await DbSeeder.ClearData(context);
+    await DbSeeder.SeedData(context);
+}
