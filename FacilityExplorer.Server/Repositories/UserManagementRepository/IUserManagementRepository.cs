576be64e567b1a438cd8158fa4200de5966949c9
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2024-01-13 17:32:29 -0800

    Initial user management set up with roles method

---
+++ FacilityExplorer.Server/Repositories/UserManagementRepository/IUserManagementRepository.cs
@@ -0,0 +1,8 @@
+﻿
+namespace FacilityExplorer.Server.Repositories.UserManagementRepository
+{
+    public interface IUserManagementRepository
+    {
+        Task<IReadOnlyList<string>> GetRolesAsync(string email);
+    }
+}
