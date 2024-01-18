8c445170942ef3bd732f300c8a4546bcf25e05df
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2024-01-18 10:17:36 -0800

    Initial controller test

---
+++ FacilityExplorer.Tests/Controllers/UserManagementControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using FacilityExplorer.Server.Controllers;
+using FacilityExplorer.Server.Repositories.UserManagementRepository;
+using NSubstitute;
+
+namespace FacilityExplorer.Tests.Controllers
+{
+    internal class UserManagementControllerTests
+    {
+        private readonly IUserManagementRepository _userManagementRepository = Substitute.For<IUserManagementRepository>();
+        private readonly UserManagementController _userManagementController;
+        public UserManagementControllerTests() => _userManagementController = new UserManagementController(_userManagementRepository);
+    }
+}
