d5e2a6cc1e505fc4d598a96ce05f7c5aa3601c34
Kfollen93 <59298462+Kfollen93@users.noreply.github.com>
2024-01-17 20:34:22 -0800

    Add check for null user and return proper response

--- FacilityExplorer.Server/Repositories/UserManagementRepository/UserManagementRepository.cs
+++ FacilityExplorer.Server/Repositories/UserManagementRepository/UserManagementRepository.cs
@@ -11,9 +11,11 @@ namespace FacilityExplorer.Server.Repositories.UserManagementRepository
             _userManager = userManager;
         }
 
-        public async Task<IReadOnlyList<string>> GetRolesAsync(string email)
+        public async Task<IReadOnlyList<string>?> GetRolesAsync(string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
+            if (user == null) return null;
+
             var rolesAssociatedWithUser = await _userManager.GetRolesAsync(user!);
             var listOfRoles = new List<string>();
             foreach (var role in rolesAssociatedWithUser)
